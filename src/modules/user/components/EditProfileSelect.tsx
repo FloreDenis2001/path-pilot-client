6e765fbc72c899c207fb48eee6d8efc82f4e1012
FloreDenis2001 <floredenis907@yahoo.com>
2024-05-20 20:27:51 +0300

    update

--- src/modules/user/components/EditProfileSelect.tsx
+++ src/modules/user/components/EditProfileSelect.tsx
@@ -14,13 +14,13 @@ const EditProfileSelect: React.FC<EditProfileSelectProps> = ({
   return (
     <div className="modal">
       <div className="modal__container">
-        <h2>Select what you want to edit</h2>
+        <h2 className="heading-primary">Select what you want to edit</h2>
 
         <div className="modal__container__profiles">
-          <button className="button intransit" onClick={handleEditCompany}>
+          <button className="button button__switch" onClick={handleEditCompany}>
             Company Profile
           </button>
-          <button className="button pending" onClick={handleEditUser}>
+          <button className="button button_switch" onClick={handleEditUser}>
             User Profile
           </button>
         </div>
